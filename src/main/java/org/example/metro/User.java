ce4f83ed716086e0cdc396ca2d9d669880ec23a0
NEW LAP <janaaaa.mohamed.123@gmail.com>
2026-05-08 15:08:39 +0300

    commit

---
+++ src/main/java/org/example/metro/User.java
@@ -0,0 +1,47 @@
+package org.example.metro;
+
+public class User {
+    private String username;
+    private String password;
+    private String fullname;
+
+    public User(String username, String password, String fullname) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname() {
+        this.fullname = fullname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{username='" + username + "', fullname='" + fullname + "'}";
+    }
+}
+
+
+
+
